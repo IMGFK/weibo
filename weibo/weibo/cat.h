973fc7699540b4db04a1f3ce9dd64e63be97332c
郭飞凯 <gfk@gfkdemacbook-pro.local>
2016-05-19 16:18:26 +0800

    创建cat类

---
+++ weibo/weibo/cat.h
@@ -0,0 +1,13 @@
+//
+//  cat.h
+//  weibo
+//
+//  Created by GFK on 16/5/19.
+//  Copyright © 2016年 GFK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cat : NSObject
+
+@end
---
+++ weibo/weibo/cat.m
@@ -0,0 +1,13 @@
+//
+//  cat.m
+//  weibo
+//
+//  Created by GFK on 16/5/19.
+//  Copyright © 2016年 GFK. All rights reserved.
+//
+
+#import "cat.h"
+
+@implementation cat
+
+@end
